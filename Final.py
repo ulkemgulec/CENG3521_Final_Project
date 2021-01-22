9855afa2893a61deccaf6887d9b9797885026c55
UlkemGulec <38588513+ulkemgulec@users.noreply.github.com>
2021-01-22 17:16:24 +0300

    small changes
    small changes

--- Final.py
+++ Final.py
@@ -40,13 +40,13 @@ warnings.filterwarnings('ignore')
 
 #We assign malignant image data to mal_images and benign image data to ben_images.
 
-mal_images = glob('skin_canser/data/train/malignant/*')
-ben_images = glob('skin_canser/data/train/benign/*')
+mal_images = glob('skin_cancer/data/train/malignant/*')
+ben_images = glob('skin_cancer/data/train/benign/*')
 
 #Also, we assign just five images to display how data looks like.
 
-mal_images_1 = glob('skin_canser/data/train/malignant/*')[:5]
-ben_images_1 = glob('skin_canser/data/train/benign/*')[:5]
+mal_images_1 = glob('skin_cancer/data/train/malignant/*')[:5]
+ben_images_1 = glob('skin_cancer/data/train/benign/*')[:5]
 
 #Length of the malignant images. This is the number of malignant images.
 
